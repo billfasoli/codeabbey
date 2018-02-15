26826a1b706a163a5681bfe1783fc402c6a7dd0a
bii <william.fasoli@gmail.com>
2018-02-14 23:00:12 -0800

    problem completed and validated 2/14/18

--- pr6.py
+++ pr6.py
@@ -3,7 +3,7 @@
 Created: 2/14/18
 Author: bii
 """
-#test
+
 #Takes size of the array as input
 array_size = int(input("Please input array size: "))
 
@@ -13,10 +13,7 @@ array = []
 #Takes
 for i in range (array_size):
     a,b = map(int, input().split())
-    if (abs((a / b)) % 1 >= 0.5):
-        array.append(round(a / b))
-    else:
-        array.append(a / b)
+    array.append(round(a / b))
 
 print()
 
