e0f61d87b64cccb11bbe23cc18351948164c0854
bii <william.fasoli@gmail.com>
2018-02-15 07:10:28 -0800

    WJF 2/15/17: Some light morning work

--- pr7.py
+++ pr7.py
@@ -4,4 +4,13 @@ Created: 2/14/18
 Author: bii
 """
 
-#Miriam is beautiful2
+#Takes size of the array as input
+array_size = int(input("Please input array size: "))
+
+#Initializes an empty array named 'array'
+a = map(int, input().split())
+
+#Takes
+for i in range (1, array[0]):
+     = map(int, input().split())
+    array.append(round()
